f6fa55b61ce654bc319d60b8bde09537d02e2dfc
lnx-mwita <coderlenovo@gmail.com>
2020-07-11 12:10:53 +0300

    second commit

--- weather/settings.py
+++ weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&ql@ia=hp45^*m-od#^1+k*yfc5q8o^^(#imt1$=(5qsu!wsy0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['myjjapp.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['weather--cast.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
